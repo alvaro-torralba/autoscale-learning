6112691b67a64b877e826ac34234509dbfc51d17
Alvaro Torralba <alto@cs.aau.dk>
2024-07-12 14:25:30 +0200

    Added scripts

---
+++ experiment-lama.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --mail-user=alto@cs.aau.dk
+#SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
+#SBATCH --partition=naples
+#SBATCH --mem-per-cpu=9000M
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=autoscale
+#SBATCH --output=/nfs/home/cs.aau.dk/bx56lg/autoscale-learning/slurm-lama.log
+#SBATCH --error=/nfs/home/cs.aau.dk/bx56lg/autoscale-learning/slurm-lama.err
+#SBATCH --array=1-27
+
+
+ulimit -v 9000000
+
+#mkdir -p /nfs/home/cs.aau.dk/bx56lg/autoscale-learning/data-lama/
+mkdir -p /scratch/alto/lama
+
+declare -a SHUFFLED_TASK_IDS=(barman blocksworld childsnack data-network depots driverlog elevators floortile grid gripper hiking logistics miconic nomystery openstacks parking rovers satellite scanalyzer snake storage termes tpp transport visitall woodworking zenotravel)
+
+DOMAIN=${SHUFFLED_TASK_IDS[$SLURM_ARRAY_TASK_ID - 1]}
+
+time ./generate-training-data.py $DOMAIN --cpus 1 --batch 1 --planner-config lama --planner-desired-lower-time 30 --output /scratch/alto/lama  > /nfs/home/cs.aau.dk/bx56lg/autoscale-learning/log_lama_$DOMAIN
+
+mv /scratch/alto/lama/$DOMAIN /nfs/home/cs.aau.dk/bx56lg/autoscale-learning/data-lama/$DOMAIN
